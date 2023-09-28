45db5d7cc4bb4172fe6e2754d2d76e6f338d1561
Phuong Pham <phuong.mom33@gmail.com>
2023-09-28 15:08:35 +0700

    login vs register

---
+++ src/components/template/RegisterTemplate.tsx
@@ -0,0 +1,17 @@
+
+
+export const RegisterTemplate = () => {
+  return (
+    <form action="">
+      <div>
+        <label htmlFor="">Tài khoản</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">Mật khẩu</label>
+        <input type="password" />
+      </div>
+      <button>Đăng ký</button>
+    </form>
+  )
+}
