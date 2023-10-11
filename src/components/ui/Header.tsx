efbd84e7d5166723c46191f8c964e1b691ebe75f
HuyNguyen081099 <huy081099@gmail.com>
2023-10-12 00:23:43 +0700

    hoàn thành chức năng Detail khoá học

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -87,7 +87,7 @@ export const Header = () => {
                     <div className={cn("items-center justify-between md:block phone:hidden w-full md:w-auto md:order-1")} id="navbar-user">
                         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
-                                <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" onClick={()=>{
+                                <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" onClick={() => {
                                     navigate('/')
                                 }}>Trang chủ</a>
                             </li>
@@ -102,7 +102,6 @@ export const Header = () => {
                             </li>
                         </ul>
                     </div>
-
                 </div>
             </nav>
         </div>
--- src/constant/config.ts
+++ src/constant/config.ts
@@ -4,4 +4,5 @@ export const PATH = {
     account: '/account',
     admin: '/admin',
     home: '/home',
+    DetailKhoaHoc : '/detail/:idKhoaHoc',
 }
